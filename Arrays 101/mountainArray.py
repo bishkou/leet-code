64c3acf5a9982d628a67ac52c0a1077e14d13e77
bishkou <chedyhammami@yahoo.com>
2020-11-07 23:43:25 +0100

    mountain

--- Arrays 101/mountainArray.py
+++ Arrays 101/mountainArray.py
@@ -3,40 +3,12 @@ from typing import List
 
 
 def validMountainArray(A: List[int]) -> bool:
-    if len(A) < 3:
-        return False
-    else:
-        i = 0
-        j = len(A) - 1
-        k = 0
-        m = 0
-
-        while i < j:
-            if (A[i] < A[i + 1]):
-                if k > 0:
-                    return False
-                else:
-                    i += 1
-                    m += 1
-
-            elif (A[i] == A[i + 1] and m > 0):
-                i += 1
-
-            elif (A[i] > A[i + 1]):
-                print(m)
-                print(A[i])
-                if m > 0:
-                    k += 1
-                    i += 1
-                else:
-                    return False
-            else:
-                return False
-
-        if (k > 0):
-            return True
-        else:
-            return False
+    i, j, n = 0, len(A) - 1, len(A)
+    while i + 1 < n and A[i] < A[i + 1]:
+        i += 1
+    while j > 0 and A[j - 1] > A[j]:
+        j -= 1
+    return 0 < i == j < n - 1
 
 
 if __name__ == '__main__':
