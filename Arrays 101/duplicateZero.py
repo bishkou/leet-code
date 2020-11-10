4cbe97fc173917f9f41eb3d4494f62441cd5d393
bishkou <chedyhammami@yahoo.com>
2020-11-10 22:03:53 +0100

    dupilcate fix

--- Arrays 101/duplicateZero.py
+++ Arrays 101/duplicateZero.py
@@ -8,7 +8,8 @@ def duplicateZeros(arr: List[int]) -> None:
         aux.append(i)
         if i == 0:
             aux.append(i)
-
+    for i in range(len(arr)):
+        arr[i] = aux[i]
 
 if __name__ == '__main__':
     nums = [1,0,2,3,0,4,5,0]
