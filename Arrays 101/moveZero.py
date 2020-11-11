6cd38ab5ee370256ef6046b1e05db0a194731986
bishkou <chedyhammami@yahoo.com>
2020-11-11 22:01:54 +0100

    move zereos

---
+++ Arrays 101/moveZero.py
@@ -0,0 +1,29 @@
+
+from typing import List
+
+
+def moveZeroes(nums: List[int]) -> None:
+    """
+    Do not return anything, modify nums in-place instead.
+    """
+    j = 0
+    i = 0
+    while i < len(nums):
+        while j < len(nums) and nums[j] == 0:
+            j += 1
+        if len(nums) > j != i:
+            temp = nums[j]
+            if nums[i] == 0 and j > i:
+                nums[i] = temp
+                nums[j] = 0
+                j += 1
+            i += 1
+        else:
+            i += 1
+            pass
+
+if __name__ == '__main__':
+    nums = [1,0,1,0,3,12]
+    moveZeroes(nums)
+    # print(moveZeroes(nums))
+    print(nums)
