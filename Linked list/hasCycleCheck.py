1e45b05442d424dde208389935cafe5f88eb0d68
bishkou <chedyhammami@yahoo.com>
2020-12-09 18:37:49 +0100

    has cycle

--- Linked list/hasCycleCheck.py
+++ Linked list/hasCycleCheck.py
@@ -1,19 +1,16 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 class Solution:
     def hasCycle(self, head: ListNode) -> bool:
-        pos = 1
-        if pos < 0:
-            return False
-        i = 0
-
-        while i < pos and head.next != None:
-            head = head.next
-            i += 1
-
-        if i == pos:
-            return True
+        slow = head
+        fast = head
+        while (slow and fast and fast.next):
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
