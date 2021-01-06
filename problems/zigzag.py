2c1620c64c6e3387005b9afd68e48652754daff8
bishkou <chedyhammami@yahoo.com>
2021-01-07 00:32:25 +0100

    zigzag

---
+++ problems/zigzag.py
@@ -0,0 +1,14 @@
+# Declaring rows
+N = 5
+
+# Declaring columns
+M = 4
+
+# using list comprehension
+# to initializing matrix
+res = [[0 for i in range(M)] for j in range(N)]
+
+# printing result
+zzString = ["a"]*4
+
+print("".join(zzString))
