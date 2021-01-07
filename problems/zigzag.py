61614b7fbda4647b3b97a786d8f1e32272c072fc
bishkou <chedyhammami@yahoo.com>
2021-01-07 17:55:34 +0100

    zigzag

--- problems/zigzag.py
+++ problems/zigzag.py
@@ -1,14 +1,21 @@
-# Declaring rows
-N = 5
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
 
-# Declaring columns
-M = 4
+        row = 0
+        if numRows == 1:
+            return s
+        down = True
+        zzString = [""] * numRows
+        for i in range(len(s)):
+            zzString[row] = zzString[row] + s[i]
+            if down:
+                row += 1
+                if row == numRows - 1:
+                    down = False
+            else:
+                row -= 1
+                if row == 0:
+                    down = True
 
-# using list comprehension
-# to initializing matrix
-res = [[0 for i in range(M)] for j in range(N)]
+        return "".join(zzString)
 
-# printing result
-zzString = ["a"]*4
-
-print("".join(zzString))
